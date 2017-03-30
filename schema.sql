15a407f2c0c007f4836a9949ecc98ab3dc8773fa
Ryan Birmingham <birm@rbirm.us>
2017-03-30 16:30:42 -0400

    try again to fix schema

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 /*queue*/
 CREATE TABLE queue
- (
+  (
     `id`          INT auto_increment NOT NULL,
     `priority`    INT DEFAULT 0,
     `type`        VARCHAR(64) NOT NULL,
@@ -8,25 +8,25 @@ CREATE TABLE queue
     `max`         VARCHAR(64),
     `started` TIMESTAMP,
     `finished` TIMESTAMP,
-    PRIMARY KEY ('id')
- );
+    PRIMARY KEY (id)
+  );
 
 CREATE TABLE errors
- (
-  `id`     INT auto_increment NOT NULL,
-  `state`  VARCHAR(64) NOT NULL,
-  `error`  TEXT,
-  `source` INT NOT NULL,
-  PRIMARY KEY ('id')
- );
+  (
+    `id`     INT auto_increment NOT NULL,
+    `state`  VARCHAR(64) NOT NULL,
+    `error`  TEXT,
+    `source` INT NOT NULL,
+    PRIMARY KEY (id)
+  );
 
 /*FetchLog*/
 CREATE TABLE fetchlog
- (
+  (
     `id`           INT auto_increment NOT NULL,
     `recordsadded` INT NOT NULL DEFAULT 0,
     `timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `source`       INT NOT NULL,
     FOREIGN KEY (source) references queue(id) on delete cascade,
-    PRIMARY KEY ( 'id' )
- );
+    PRIMARY KEY (id)
+  );
